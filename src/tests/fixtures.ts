4034d44dc4d2642d70787684eea5a3dddfa3ff96
Andrew Musgrave <andrew.musgrave@shopify.com>
2020-02-19 00:53:26 -0500

    :white_check_mark: Added test coverage

--- src/tests/fixtures.ts
+++ src/tests/fixtures.ts
@@ -223,3 +223,106 @@ export const mockPropertyDeclaration = {
   ],
   start: mockStart,
 };
+
+export const mockCustomPropertyErrors = {
+  '--p-border-radius-base': {
+    declaration: false,
+    usedFromDeclaration: false,
+    count: 1,
+    locations: [
+      {
+        start: mockStart,
+        end: mockNext,
+        file: 'src/components/ActionList/ActionList.scss',
+      },
+    ],
+  },
+};
+
+export const mockCustomProperties = {
+  '--Polaris-RangeSlider-progress-lower': {
+    declaration: false,
+    usedFromDeclaration: false,
+    count: 4,
+    locations: [
+      {
+        start: mockStart,
+        end: mockNext,
+        file: 'src/components/RangeSlider/components/DualThumb/DualThumb.scss',
+      },
+      {
+        start: mockStart,
+        end: mockNext,
+        file: 'src/components/RangeSlider/components/DualThumb/DualThumb.scss',
+      },
+      {
+        start: mockStart,
+        end: mockNext,
+        file: 'src/components/RangeSlider/components/DualThumb/DualThumb.scss',
+      },
+      {
+        start: mockStart,
+        end: mockNext,
+        file: 'src/components/RangeSlider/components/DualThumb/DualThumb.scss',
+      },
+    ],
+  },
+  '--Polaris-RangeSlider-progress-upper': {
+    declaration: false,
+    usedFromDeclaration: false,
+    count: 4,
+    locations: [
+      {
+        start: mockStart,
+        end: mockNext,
+        file: 'src/components/RangeSlider/components/DualThumb/DualThumb.scss',
+      },
+      {
+        start: mockStart,
+        end: mockNext,
+        file: 'src/components/RangeSlider/components/DualThumb/DualThumb.scss',
+      },
+      {
+        start: mockStart,
+        end: mockNext,
+        file: 'src/components/RangeSlider/components/DualThumb/DualThumb.scss',
+      },
+      {
+        start: mockStart,
+        end: mockNext,
+        file: 'src/components/RangeSlider/components/DualThumb/DualThumb.scss',
+      },
+    ],
+  },
+  '--progress-lower': {
+    declaration: true,
+    usedFromDeclaration: false,
+    count: 4,
+    locations: [
+      {
+        start: mockStart,
+        end: mockNext,
+        file:
+          'src/components/RangeSlider/components/SingleThumb/SingleThumb.scss',
+      },
+      {
+        start: mockStart,
+        end: mockNext,
+        file:
+          'src/components/RangeSlider/components/SingleThumb/SingleThumb.scss',
+      },
+      {
+        start: mockStart,
+        end: mockNext,
+        file:
+          'src/components/RangeSlider/components/SingleThumb/SingleThumb.scss',
+      },
+      {
+        start: mockStart,
+        end: mockNext,
+        file:
+          'src/components/RangeSlider/components/SingleThumb/SingleThumb.scss',
+      },
+    ],
+  },
+};
